cca31d03dbd3309768f2ea357bd3370a49725be8
ilya <011283im@gmail.com>
2022-08-21 12:44:05 +0300

    начинаем подоготовку к сохранению текущей темы в шеред преференс

---
+++ app/src/main/java/com/example/nasacustomapp/model/theme/AppTheme.kt
@@ -0,0 +1,6 @@
+package com.example.nasacustomapp.model.theme
+
+enum class AppTheme {
+    LIGHT,CUSTOM,DARK
+
+}
--- app/src/main/java/com/example/nasacustomapp/utils/AppConst.kt
+++ app/src/main/java/com/example/nasacustomapp/utils/AppConst.kt
@@ -2,3 +2,6 @@ package com.example.nasacustomapp.utils
 
 const val  NASA_BASE_URL = "https://api.nasa.gov/"
 const val  WIKI_PARSE_URL = "https://en.wikipedia.org/wiki/"
+
+const val APPLICATION_PREFS_THEME_FILE = "theme.properties"
+const val APP_THEME_TAG = "APP_THEME_TAG"
