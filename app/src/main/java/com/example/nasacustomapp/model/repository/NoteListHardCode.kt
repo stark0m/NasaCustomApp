31c0bc48b0c96e98eac5b8cb9d397e8a2c7ff667
ilya <011283im@gmail.com>
2022-10-18 13:41:35 +0300

    задание  6/ добавили макет длинной заметки

--- app/src/main/java/com/example/nasacustomapp/model/repository/NoteListHardCode.kt
+++ app/src/main/java/com/example/nasacustomapp/model/repository/NoteListHardCode.kt
@@ -1,13 +1,17 @@
 package com.example.nasacustomapp.model.repository
 
+import android.content.res.Resources
+import android.provider.Settings.Global.getString
 import com.example.nasacustomapp.R
+import com.example.nasacustomapp.utils.LONG_TEXT
 import com.example.nasacustomapp.utils.Note
 import com.example.nasacustomapp.utils.NoteType
 
 class NoteListHardCode:NoteList {
     override fun getNoteList(): List<Note> {
+        val resources =
         return listOf(
-//            Note("first note", noteText = R.string.long_text.toString(), noteType = NoteType.TEXT_LONG),
+            Note("first note", noteText = LONG_TEXT, noteType = NoteType.TEXT_LONG),
             Note("second note","Verylong text"),
             Note("3 note","Verylong text"),
             Note("4 note","Verylong text"),
--- app/src/main/java/com/example/nasacustomapp/view/NotesPackage/rvAdapter.kt
+++ app/src/main/java/com/example/nasacustomapp/view/NotesPackage/rvAdapter.kt
@@ -4,34 +4,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.nasacustomapp.databinding.LongNoteLayoutBinding
 import com.example.nasacustomapp.databinding.SmallNoteLayoutBinding
 import com.example.nasacustomapp.utils.Note
+import com.example.nasacustomapp.utils.NoteType
 
 class rvAdapter(private val listData: List<Note>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<rvAdapter.BaseViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val binding = SmallNoteLayoutBinding.inflate(LayoutInflater.from(parent.context))
-        return SmallNoteViewHolder(binding)
+    override fun getItemViewType(position: Int): Int {
+        return listData[position].noteType
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return when (viewType) {
+            NoteType.TEXT_SHORT -> {
+                val binding = SmallNoteLayoutBinding.inflate(LayoutInflater.from(parent.context))
+                SmallNoteViewHolder(binding)
+            }
+            else -> {
+                val binding = LongNoteLayoutBinding.inflate(LayoutInflater.from(parent.context))
+                LargeNoteViewHolder(binding)
+            }
+        }
+
+
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as SmallNoteViewHolder).bind(listData[position])
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bind(listData[position])
     }
 
     override fun getItemCount(): Int = listData.size
 
     class SmallNoteViewHolder(val binding: SmallNoteLayoutBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        BaseViewHolder(binding.root) {
 
-        fun bind(note: Note) {
+        override fun bind(note: Note) {
             binding.textNote.text = note.noteText
             binding.descriptionNote.text = note.description
         }
 
     }
 
+    class LargeNoteViewHolder(val binding: LongNoteLayoutBinding) :
+        BaseViewHolder(binding.root) {
 
+        override fun bind(note: Note) {
+            binding.textNote.text = note.noteText
+            binding.descriptionNote.text = note.description
+        }
 
+    }
 
+    abstract class BaseViewHolder(view: View) :
+        RecyclerView.ViewHolder(view) {
+        abstract fun bind(note: Note)
+    }
 }
